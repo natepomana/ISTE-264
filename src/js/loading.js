e766a23e16e081243243df3ee3c99d2f9a59c985
Nathan Pomana <nap7366@rit.edu>
2018-02-09 10:56:45 -0500

    Basic development of Loading.js

---
+++ src/js/loading.js
@@ -0,0 +1,35 @@
+(function ($) {
+	/**
+	* Welcome to Loading.js!
+	*/
+
+	// Create default loader.
+	$.fn.loading = function(data){	
+		switch (data.type){
+			case "circle":
+				circle(this,data);
+				break;
+			default:
+				console.error("Missing type. (ex: type: 'circle')")
+		}
+
+		
+	};
+
+	function circle(ele,data){
+		// max data.weight is half of data.size.
+		$(ele).css({
+			'border' : data.weight+' solid'+data.colors[0],
+			'border-radius' : '50%',
+			'border-top' : data.weight+' solid '+data.colors[1],
+			'width' : data.size,
+			'height' : data.size,
+			'-webkit-animation' : 'spin '+data.speed+'s linear infinite'
+		});
+	}
+
+
+
+
+
+}(jQuery));
