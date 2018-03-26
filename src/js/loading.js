0678b174452b8b6e3bf8ec5fa5bb4ca1e736cd70
Nathan Pomana <nap7366@rit.edu>
2018-03-25 21:26:06 -0400

    HW 3 - complete
    Finishing touches.

--- src/js/loading.js
+++ src/js/loading.js
@@ -9,6 +9,9 @@
 			case "circle":
 				circle(this,data);
 				break;
+			case "line":
+				line(this,data);
+				break;
 			default:
 				console.error("Missing type. (ex: type: 'circle')")
 		}
@@ -26,6 +29,33 @@
 			'height' : data.size,
 			'-webkit-animation' : 'spin '+data.speed+'s linear infinite'
 		});
+	}
+
+	function line(ele,data){
+		switch (data.style){
+			case "line":
+				var div = $('<div />').addClass('line').css({
+					'width': data.length,
+					'height': data.height
+				});
+				//length of
+				var dec = data.time/data.count;
+				while (data.count > 0){
+					var temp = $('<div />').css({
+						'-webkit-animation-delay': "-" + (data.time-dec)*2 + "s",
+						'animation-delay': "-" +  (data.time-dec)*2 + "s",
+						'width': data.width,
+						'margin-right': data.width.substring(0,data.width.length-2)/2 + "px",
+					});
+					$(div).append(temp);
+					data.count--;
+					data.time -= dec;
+				}
+				$(ele).append(div);
+				break;
+			case "dot":
+				break;
+		}
 	}
 
 
